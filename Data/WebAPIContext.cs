a99990ca838f15aab78e3289c6c019e15ae57752
SEBASTIAN-RAREȘ VARGA <sebastian.varga@stud.ubbcluj.ro>
2023-12-18 19:03:49 +0200

    Add project files.

---
+++ Data/WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Varga_Sebastian_Lab11.Models;
+
+namespace Varga_Sebastian_Lab11.Data
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Varga_Sebastian_Lab11.Models.ShopList> ShopList { get; set; } = default!;
+    }
+}
